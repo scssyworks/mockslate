566352b0dc486c9b7d8391701fb8c48336cd915e
Sachin Singh <contactsachinsingh@gmail.com>
2023-02-03 11:56:37 +0530

    Optimization changes

--- server.js
+++ server.js
@@ -2,6 +2,5 @@ const server = require('./config/server');
 const resolver = require('./modules/resolver');
 
 server(({ cache, key, query, body }) => {
-  const { code, response } = resolver(cache, key, { query, body });
-  return { code, response };
+  return resolver(cache, key, { query, body });
 });
